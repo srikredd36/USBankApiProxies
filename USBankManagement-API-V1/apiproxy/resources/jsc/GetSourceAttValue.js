cbe6ec04e087832ba45d3e1d00ea09f2b05bfc01
Divya Sharma <dsharma55@sapient.com>
2017-07-05 08:54:49 +0530

    Added Error Handling

--- USBankManagement-API-V1/apiproxy/resources/jsc/GetSourceAttValue.js
+++ USBankManagement-API-V1/apiproxy/resources/jsc/GetSourceAttValue.js
@@ -1,7 +1,12 @@
 var brandSourceValueKVM=context.getVariable("QueryString.brandSourceValue");
 var brandSourceValueJSON=JSON.parse(brandSourceValueKVM);
-var brandName=(context.getVariable("brandName")).toLowerCase();
-var brandSourceValue=brandSourceValueJSON[brandName];
-if(brandSourceValue === undefined)
+
+var brandName=(context.getVariable("brandName"))
+if(brandName === undefined)
+throw error;
+
+var brandSourceValue=brandSourceValueJSON[brandName.toLowerCase()];
+if (brandSourceValue === undefined)
 throw error;
 context.setVariable("QueryString.value",brandSourceValue);
+
